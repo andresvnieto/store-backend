5835a3635502802512688b3f0c7a97b1e6c2efcb
andresvnieto <andreswd001@gmail.com>
2023-10-08 20:23:01 -0500

    Todo queda modularizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { UsersController } from './controllers/users.controller';
-import { OrdersController } from './controllers/orders.controller';
-import { CustomersController } from './controllers/customers.controller';
-import { BrandsController } from './controllers/brands.controller';
-import { ProductsService } from './services/products.service';
-import { CategoriesService } from './services/categories.service';
-import { OrdersService } from './services/orders.service';
-import { UsersService } from './services/users.service';
-import { CustomersService } from './services/customers.service';
-import { BrandsService } from './services/brands.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BrandsModule } from './brands/brands.module';
+import { CustomersModule } from './customers/customers.module';
 @Module({
-  imports: [UsersModule, ProductsModule],
-  controllers: [
-    AppController,
-    ProductsController,
-    CategoriesController,
-    UsersController,
-    OrdersController,
-    CustomersController,
-    BrandsController,
+  imports: [
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+    CategoriesModule,
+    BrandsModule,
+    CustomersModule,
   ],
-  providers: [
-    AppService,
-    ProductsService,
-    CategoriesService,
-    OrdersService,
-    UsersService,
-    CustomersService,
-    BrandsService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
